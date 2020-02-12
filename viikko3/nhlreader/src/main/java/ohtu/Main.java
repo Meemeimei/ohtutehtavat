6d8c1d55e1345f700564506f85a5cab4d0f1d60b
Eemi <eemi.tantarimaki@cs.helsinki.fi>
2020-02-12 23:26:01 +0200

    viikko3

---
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -0,0 +1,41 @@
+package ohtu;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import org.apache.http.client.fluent.Request;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+        
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+                
+        //System.out.println("json-muotoinen data:");
+        //System.out.println( bodyText );
+        
+        System.out.println("Players from FIN");
+        
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+        
+        //System.out.println("Oliot:");
+        System.out.println("");
+        ArrayList<Player> playerList = new ArrayList<>();
+        
+        for (Player player : players) {
+            if (player.getNationality().equals("FIN")) {
+                playerList.add(player);
+            }
+        }   
+        
+        Collections.sort(playerList);
+        
+        for (Player player : playerList) {
+            System.out.println(player.toString());
+        }
+        
+    }
+  
+}
---
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,83 @@
+
+package ohtu;
+
+public class Player implements Comparable<Player> {
+    private String name;
+    private int goals;
+    private int assists;
+    private int penalties;
+    private String team;
+    private String nationality;
+    private String birthdate;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+    
+    public int getGoals() {
+        return goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getPoints() {
+        return goals + assists;
+    }
+    
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+   
+    
+    @Override
+    public String toString() {
+        return name + "     \t" + team + "\t" + goals + " + " + assists + " = " + getPoints();
+    }
+    
+    @Override
+    public int compareTo(Player anotherPlayer) {
+        return anotherPlayer.getPoints() - this.getGoals();
+    }
+}
